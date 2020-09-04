82a4e806995dbe37c2c7f68de8ec546c346576f4
Ruthenium74 <Ruthenium74@yandex.ru>
2020-09-04 17:30:01 +0500

    Add test for create duplicate restaurants with same name

--- src/test/java/ru/ruthenium74/voteforrestaurant/web/restaurant/AdminRestaurantRestControllerTest.java
+++ src/test/java/ru/ruthenium74/voteforrestaurant/web/restaurant/AdminRestaurantRestControllerTest.java
@@ -5,7 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import ru.ruthenium74.voteforrestaurant.RestaurantTestData;
+import ru.ruthenium74.voteforrestaurant.exception.ErrorType;
 import ru.ruthenium74.voteforrestaurant.model.Restaurant;
 import ru.ruthenium74.voteforrestaurant.repository.CrudRestaurantRepository;
 import ru.ruthenium74.voteforrestaurant.web.AbstractRestControllerTest;
@@ -14,6 +17,7 @@ import ru.ruthenium74.voteforrestaurant.web.json.JsonUtil;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static ru.ruthenium74.voteforrestaurant.RestaurantTestData.RESTAURANT_MATCHER;
 import static ru.ruthenium74.voteforrestaurant.TestUtil.readFromJson;
+import static ru.ruthenium74.voteforrestaurant.util.ValidationUtil.RESTAURANT_UNIQUE_NAME_CONSTRAINT;
 
 public class AdminRestaurantRestControllerTest extends AbstractRestControllerTest {
 
@@ -34,5 +38,23 @@ public class AdminRestaurantRestControllerTest extends AbstractRestControllerTes
         newRestaurant.setId(id);
         RESTAURANT_MATCHER.assertMatch(created, newRestaurant);
         RESTAURANT_MATCHER.assertMatch(restaurantRepository.findById(id).orElseThrow(), newRestaurant);
+    }
+
+    @Test
+    @Transactional(propagation = Propagation.NEVER)
+    void createDuplicate() throws Exception {
+        Restaurant newRestaurant = RestaurantTestData.getNew();
+
+        perform(MockMvcRequestBuilders.post(AdminRestaurantRestController.REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(newRestaurant)))
+                .andExpect(status().isCreated());
+
+        perform(MockMvcRequestBuilders.post(AdminRestaurantRestController.REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(newRestaurant)))
+                .andExpect(status().isConflict())
+                .andExpect(errorType(ErrorType.VALIDATION_ERROR))
+                .andExpect(detailMessage(RESTAURANT_UNIQUE_NAME_CONSTRAINT));
     }
 }
